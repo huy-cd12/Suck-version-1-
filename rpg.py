e1eeef58ecb4fd25661626bd59d0fb3b46d5484a
huy-cd12 <bachduchuy2001@gmail.com>
2026-06-11 23:21:04 +1000

    better a little bit

--- rpg.py
+++ rpg.py
@@ -13,61 +13,42 @@ hit_mons = [10, 22, 13,]
 print('who gonna first?')
 turn = input().strip().lower()
 
+while turn != hero and turn != mons:
+    print("wrong input")
+    turn = input().strip().lower()
 
 while hero_hp > 0 and mons_hp > 0:
     count += 1
-    if turn == hero:
-        
-        print (f"turn {count}") 
-        dmg = random.choice(hit_hero)
-        print (f"hero deal {dmg} ")
-        mons_hp -= dmg
-
-        if mons_hp > 0:
-              print(f"hp's monster: {mons_hp}")
-              print("----------------------")
-              print (f"turn {count}")
-              dmg = random.choice(hit_mons)
-              print (f"monster deal {dmg} ")
-              hero_hp -= dmg
-              print(f"hp's hero: {hero_hp}")
-
-        else:
-             print("monster lose")
-             break
-
-            
-    elif turn == mons:
-            
-            print (f"turn {count}") 
-            dmg = random.choice(hit_mons)
-            print (f"monster deal {dmg} ")
-            hero_hp -= dmg
-            
-
-            if hero_hp > 0:
-                 print(f"hp's hero: {hero_hp}")
-                 print("--------------------")
-                 dmg = random.choice(hit_hero)
-                 print (f"hero deal {dmg} ")
-                 mons_hp -= dmg
-                 print(f"hp's monster: {mons_hp}")   
-            else:
-                 print("hero lose")
-                 break
-
-    else:
-        print("wrong input")
-        while True:
-            turn = input().strip().lower()
-            if turn == hero or turn == mons:
-                 break
-            else:
-                 print("wrong input")
-            
-
-    if hero_hp <=0 or mons_hp <= 0:
-        break    
+    print(f"\nTurn {count}")
     
+    if turn == hero:
+        dmg = random.choice(hit_hero)
+        print(f"Hero deal {dmg}")
+        mons_hp -= dmg
+        
+        if mons_hp <=0:
+            print()
+            print("monster = 0")
+            print("monster lose")
+            break
+        
+        print(f"Monster's hp {mons_hp}")
+        turn = mons
+        
+    elif turn == mons:
+        dmg = random.choice(hit_mons)
+        print(f"Monster deal {dmg}")
+        hero_hp -= dmg
 
+        if hero_hp <= 0:
+            print("----------------------")
+            print("Hero = 0")
+            print("Hero lose")
+            break
+        
+        print (f"hero's hp {hero_hp}")
+        turn = hero
+
+
+    print("----------------------")
 
